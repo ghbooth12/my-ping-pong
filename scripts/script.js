73532b7b086733b91e15d6e6c9dda2094d962187
Gahee Heo <ghbooth12@gmail.com>
2016-08-05 09:19:16 -0500

    ch2-create-visuals-fix: completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,115 +1,75 @@
 window.onload = function() {
   render();
-  movePaddle();
-  // animate(step);
 };
 
 var tableCanv = document.getElementById('table');
 var ctx = tableCanv.getContext('2d');
-var widthCanv = tableCanv.getAttribute('width');
-var heightCanv = tableCanv.getAttribute('height');
+tableCanv.width = tableCanv.getAttribute('width');
+tableCanv.height = tableCanv.getAttribute('height');
 
+// Build objects
 var computer = new Computer();
 var player = new Player();
 var ball = new Ball();
-// var paddle = new Paddle();
 
-function table() {
-  ctx.fillStyle = '#8fc5f1';
-  ctx.fillRect(0, 0, widthCanv, heightCanv);
-}
-
-function Computer() {
-  this.color = '#662200';
-  this.posX = 250;
-  this.posY = 0;
-  this.width = 100;
-  this.height = 20;
-  this.render = function() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.posX, this.posY, this.width, this.height);
-  };
-}
-
-function Player() {
-  this.color = '#662200';
-  this.posX = 250;
-  this.posY = 480;
-  this.width = 100;
-  this.height = 20;
-  this.render = function() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.posX, this.posY, this.width, this.height);
-  };
-}
-
-// function Paddle() {
-//   this.speed = 10;
-//   this.move = function() {
-//     window.addEventListener('keypress', function(event) {
-//       if (event.keyCode == 37) {
-//         console.log("left arrow");
-//         player.posX -= 10;
-//       } else if (event.keyCode == 39) {
-//         console.log("right arrow");
-//         player.posY += 10;
-//       }
-//     });
-//   };
-// }
-
-function Ball() {
-  this.color = '#ffff00';
-  this.posX = 300;
-  this.posY = 30;
-  this.radius = 10;
-  this.render = function() {
-    ctx.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  };
-}
-
-// var animate = window.requestAnimationFrame ||
-//         window.webkitRequestAnimationFrame ||
-//         window.mozRequestAnimationFrame    ||
-//         window.oRequestAnimationFrame      ||
-//         window.msRequestAnimationFrame     ||
-//         function(callback) { window.setTimeout(callback, 1000/60) };
-//
-// function step() {
-//   movePaddle();
-//   animate(step);
-// }
 
 function render() {
-  table();
+  // Render Table
+  ctx.fillStyle = '#8fc5f1';
+  ctx.fillRect(0, 0, tableCanv.width, tableCanv.height);
+
   computer.render();
+  player.render();
   ball.render();
 }
 
-function movePaddle() {
-  var speed = 50;
-  var leftKey = 44;
-  var rightKey = 46;
-  player.render();
-
-  window.addEventListener('keypress', function(e) {
-    console.log(e.keyCode); // For testing
-    if (e.keyCode == leftKey) {
-      ctx.clearRect(player.posX, player.posY, player.width, player.height);
-      render();
-      if (player.posX >= player.width / 2) {
-        player.posX -= speed;
-      }
-      player.render();
-    } else if (e.keyCode == rightKey) {
-      ctx.clearRect(player.posX, player.posY, player.width, player.height);
-      render();
-      if (player.posX < (widthCanv - player.width)) {
-        player.posX += speed;
-      }
-      player.render();
-    }
-  });
+// Object Constructor: Paddle
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
 }
+
+Paddle.prototype.render = function() {
+  ctx.fillStyle = '#662200';
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+
+
+// Object Constructor: Computer
+function Computer() {
+  this.paddle = new Paddle(250, 0, 100, 20);
+}
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
+
+// Object Constructor: Player
+function Player() {
+  this.paddle = new Paddle(250, 480, 100, 20);
+}
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+
+// Object Constructor: Ball
+function Ball() {
+  this.x = 300;
+  this.y = 30;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 10;
+}
+
+Ball.prototype.render = function() {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+  ctx.fillStyle = '#ffff00';
+  ctx.fill();
+};
